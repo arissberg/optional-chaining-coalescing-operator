2bb9a6968105a4f4611ad5d82887dbfcf89b690f
Steve Ariss <steve@arissberg.com>
2020-04-08 14:58:25 -0400

    minor

--- src/index.js
+++ src/index.js
@@ -6,14 +6,16 @@ import people from "./people";
 const container = document.querySelectorAll(".container")[0];
 
 people.forEach(person => {
-  const name = `${person?.firstName ?? "John"} ${person?.lastName ?? "Smith"}`;
-  const weight = `${person?.stats?.weight ?? "N/A"} lbs`;
+  const name = `${person?.firstName ?? "John"} ${person?.lastName ?? "Doe"}`;
+  const weight = `${person?.stats?.weight ?? "Uknown"} lbs`;
 
   container.innerHTML += `
   <div class="card ${person?.gender}">
-    <div class="avatar"><img src="https://api.adorable.io/avatars/90/${name}@adorable.io.png" alt="${name}" /></div>
+    <div class="avatar">
+      <img src="https://api.adorable.io/avatars/90/${name}@adorable.io.png" alt="${name}" />
+    </div>
     <h3>${name}</h3>
-    <p>🎂 ${person?.stats?.age ?? "N/A"}</p>
+    <p>🎂 ${person?.stats?.age ?? 'N/A'}</p>
     <p>⚖ ${weight}</p>
   </div>
   `;
